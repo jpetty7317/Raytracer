1dbcb9cabc68dcc8b1632b6934d232243145a5c2
John Petty <john@marxentlabs.com>
2019-11-15 17:01:08 -0500

    Added FOV control to the camera. Also added #define _USE_MATH_DEFINES above the include of <math.h> in vec3.h to get access to the predefined maacros such as M_PI

--- Raytracer/camera.h
+++ Raytracer/camera.h
@@ -5,10 +5,13 @@
 
 class camera {
 	public:
-		camera() {
-			lowerLeftCorner = vec3(-2.0, -1.0, -1.0);
-			horizontal = vec3(4.0, 0.0, 0.0);
-			vertical = vec3(0.0, 2.0, 0.0);
+		camera(float vfov, float aspect) {
+			float theta = vfov * M_PI / 180;
+			float halfHeight = tan(theta / 2);
+			float halfWidth = aspect * halfHeight;
+			lowerLeftCorner = vec3(-halfWidth, -halfHeight, -1.0);
+			horizontal = vec3(2.0 * halfWidth, 0.0, 0.0);
+			vertical = vec3(0.0, 2.0 * halfHeight, 0.0);
 			origin = vec3(0.0, 0.0, 0.0);
 		}
 		ray getRay(float u, float v) { return ray(origin, lowerLeftCorner + u * horizontal + v*vertical - origin); }
--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -42,15 +42,13 @@ int main()
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);*/
 
-	hitable *list[5];
-	list[0] = new sphere(vec3(0.0, 0.0, -1.0), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
-	list[1] = new sphere(vec3(0.0, -100.5, -1.0), 100, new lambertian(vec3(0.8,0.8,0.0)));
-	list[2] = new sphere(vec3(1.0, 0.0, -1.0), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.1));
-	list[3] = new sphere(vec3(-1.0, 0.0, -1.0), 0.5, new dielectric(1.5));
-	list[4] = new sphere(vec3(-1.0, 0.0, -1.0), -0.45, new dielectric(1.5));
-	hitableList *world = new hitableList(list, 5);
+	hitable *list[2];
+	float R = cos(M_PI / 4);
+	list[0] = new sphere(vec3(-R, 0.0, -1.0),R, new lambertian(vec3(0,0,1)));
+	list[1] = new sphere(vec3(R, 0.0, -1.0), R, new lambertian(vec3(1,0,0)));
+	hitableList *world = new hitableList(list, 2);
 
-	camera cam;
+	camera cam(90, float(nx)/float(ny));
 
 	for (int j = ny - 1; j >= 0; j--)
 	{
